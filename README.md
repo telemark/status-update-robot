11291055f07e6dd5db8e8da79171e074b2a93f2c
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-05-29 15:33:23 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+[![Build Status](https://travis-ci.org/telemark/status-update-robot.svg?branch=master)](https://travis-ci.org/telemark/status-update-robot)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+[![Greenkeeper badge](https://badges.greenkeeper.io/telemark/minelev-saksbehandler-robot.svg)](https://greenkeeper.io/)
+
 # status-update-robot
+
 Bot for updating status
+
+## License
+
+[MIT](LICENSE)
+
+![Robohash image of status-update-robot](https://robots.kebabstudios.party/minelev-saksbehandler-robot.png "Robohash image of status-update-robot")
---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const getNextJobFromQueue = require('./lib/steps/get-next-job-from-queue')
+const getFileData = require('./lib/steps/get-file-data')
+const postStatus = require('./lib/steps/post-status')
+const saveToDone = require('./lib/steps/save-to-done')
+const saveToErrors = require('./lib/steps/save-to-errors')
+const removeFromQueue = require('./lib/steps/remove-from-queue')
+const logger = require('./lib/logger')
+
+logger('info', ['index', 'start'])
+
+getNextJobFromQueue()
+  .then(getFileData)
+  .then(postStatus)
+  .then(saveToDone)
+  .then(saveToErrors)
+  .then(removeFromQueue)
+  .then((data) => {
+    logger('info', ['index', data._id, 'finished'])
+    process.exit(0)
+  })
+  .catch((error) => {
+    logger('error', ['index', 'error', JSON.stringify(error)])
+    process.exit(1)
+  })
