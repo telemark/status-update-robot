0bf78bfdfe3fb24ccbceb96a06832d18cf5480e4
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-08-22 14:21:27 +0200

    Adds prepare-data step

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const getNextJobFromQueue = require('./lib/steps/get-next-job-from-queue')
 const getFileData = require('./lib/steps/get-file-data')
+const prepareData = require('./lib/steps/prepare-data')
 const postStatus = require('./lib/steps/post-status')
 const saveToDone = require('./lib/steps/save-to-done')
 const saveToErrors = require('./lib/steps/save-to-errors')
@@ -12,6 +13,7 @@ logger('info', ['index', 'start'])
 
 getNextJobFromQueue()
   .then(getFileData)
+  .then(prepareData)
   .then(postStatus)
   .then(saveToDone)
   .then(saveToErrors)
